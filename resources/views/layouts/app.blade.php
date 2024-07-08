56d1293276919012732fd014381984a58290577c
Italo Morales Fantone <profesoritalomoralesf@gmail.com>
2024-07-07 21:49:19 -0500

    tailwindcss

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code Repository</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="bg-[#212121] text-[#eff]">
+        @yield('content')
+    </div>
+</body>
+</html>
