faf4199098dd9c07a24564857d2abf7d9c29d095
Italo Morales Fantone <profesoritalomoralesf@gmail.com>
2024-09-04 17:13:58 -0500

    WIP

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,12 @@ use App\Http\Controllers\GithubAuthenticatorController;
 use App\Http\Controllers\SnippetController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::view('/', 'snippets.create')->name('home');
+Route::view('/', 'snippets.create')->name('home');
+Route::get('{snippet}', [SnippetController::class, 'show'])->name('snippets.show');
+Route::get('{snippet}/edit', [SnippetController::class, 'edit'])->name('snippets.edit');
 
+Route::middleware('guest')->group(function () {
     Route::view('login', 'auth.login')->name('login');
-    
-    Route::get('{snippet}', [SnippetController::class, 'show'])->name('snippets.show');
-    Route::get('{snippet}/edit', [SnippetController::class, 'edit'])->name('snippets.edit');
-    
     Route::get('auth/github/redirect', [GithubAuthenticatorController::class, 'redirect'])->name('auth.github');
     Route::get('auth/github/callback', [GithubAuthenticatorController::class, 'callback'])->name('auth.github.callback');
 });
