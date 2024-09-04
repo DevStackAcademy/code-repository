fa334d465a3c5d196546298da5db04a73d6b5836
Italo Morales Fantone <profesoritalomoralesf@gmail.com>
2024-09-04 17:08:18 -0500

    refactor users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable()->unique();
             $table->string('github_id')->unique();
             $table->timestamps();
         });
